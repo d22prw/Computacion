6bb33c5fa99754a86bd0201f200fe7a5dbaf72ef
Diego <d22prw@gmail.com>
2016-12-01 08:26:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-#!/usr/bin/python
 #-*- coding: utf-8-*-
 from pymongo import MongoClient
 import urllib2, json
@@ -20,7 +19,6 @@ def valores_cotizacion():
 	lista = {}
 	for valor_db in valores_basedatos:
 		lista[valor_db['Cotizacion']]=valor_db
-
 	return render_template('valores_cotizacion.html',lista=lista)
 
 @app.route('/obtiene_sup_umbral', methods=['POST'])
@@ -35,7 +33,6 @@ def obtiene_sup_umbral():
 			print(umbral_pag)
 			dic_sup = {}
 			for datos_umbral in basedatos_umbral:
-				#if(umbral_pag<float(basedatos_umbral['Cotizacion'].replace(',','.'))):
 				if(umbral_pag<float(datos_umbral['Cotizacion'].replace(',','.'))):
 					print(datos_umbral)
 					dic_sup[datos_umbral['Cotizacion']]=datos_umbral
@@ -49,13 +46,12 @@ def media_cotizaciones():
 		cotizacion_suma = 0
 		n_datos = 0
 		for datos in datos_db:
-			cotizacion_suma = cotizacion_suma + float(datos['Cotizacion'].replace('%','').replace(',','.'))
+			cotizacion_suma = cotizacion_suma + float(datos['Cotizacion'].replace(',','.'))
 			n_datos = n_datos+1
 		media = float(cotizacion_suma/n_datos)
 	if(db_usar == 'Thingspeak'):
 		valores = urllib2.urlopen('https://thingspeak.com/channels/179436/field/1.json')
 		print(valores)
-		#datos_db = ast.literal_eval(str(valores.read()))
 		datos_leidos = valores.read()
 		print(datos_leidos)
 		datos_db = json.loads(datos_leidos)
@@ -64,11 +60,11 @@ def media_cotizaciones():
 		print(datos_db)
 		for datos in datos_db['feeds']:
 			print(datos)
-			#if datos['field1'] != 'Cotizacion':
 			cotizacion_suma = cotizacion_suma + float(datos['field1'].replace(',','.'))
 			n_datos = n_datos +1;
 		media = float(cotizacion_suma/n_datos)
 	return render_template('cotizacion_media.html', media = media)
+
 @app.route('/obtiene_graficas', methods=['POST'])
 def obtiene_graficas():
 	return render_template('graficas.html')
@@ -82,8 +78,6 @@ def login():
 	print(usuariosycontrasenas)
 	userpass= 0
 	for userpass in usuariosycontrasenas:
-	#if((str(usuario)=='admin')and(str(contrasena)=='1234')):
-	#if(existe != None):
 		if((userpass['Usuario']==str(usuario))and(userpass['Contrasena']==str(contrasena))):
 			return render_template('main.html')
 		else:
